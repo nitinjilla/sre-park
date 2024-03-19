b61c98eb2f79229abbad54904cf38148cf2383a2
nitinjilla <nitinjilla9@gmail.com>
2024-03-19 22:54:46 +0100

    changed network pod deployment method

--- k8s/vagrant/start-cluster.sh
+++ k8s/vagrant/start-cluster.sh
@@ -4,7 +4,7 @@
 
 # Starting the cluster
 
-sudo kubeadm init --apiserver-advertise-address $API_SERVER_IP --pod-network-cidr 10.5.0.0/16
+sudo kubeadm init --apiserver-advertise-address 192.168.10.10 --pod-network-cidr 10.5.0.0/16
 
 # Cluster administration will be done with vagrant user
 # Copy the kubernetes-admin config file to its home folder
@@ -13,9 +13,7 @@ mkdir -p /home/vagrant/.kube
 sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 sudo chown vagrant:vagrant /home/vagrant/.kube/config
 
-curl https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml -o kube-router.yaml
-
-kubectl apply -f kube-router.yaml
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
 #kubeadm token create --print-join-command
 # Use ^ to generate a token to join the nodes to cluster
