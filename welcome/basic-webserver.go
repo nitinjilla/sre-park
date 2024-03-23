439efd992b496146eeaa6803dfce6c6ab7b30421
nitinjilla <nitinjilla9@gmail.com>
2024-03-23 22:09:00 +0100

    added version and graceful shutdown

--- welcome/basic-webserver.go
+++ welcome/basic-webserver.go
@@ -3,33 +3,46 @@
 // Author: Nitin Jilla
 
 package main
-	
+
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/gorilla/mux"
 )
 
 var port string = ":8000"
+var version string = "v1.1" // "latest" version tag is just for fun
 
 func main() {
 
 	r := mux.NewRouter()
-
 	srv := http.Server{
 		Addr:    port,
 		Handler: r,
 	}
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
 	r.HandleFunc("/", homePage)
 
-	err := srv.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-	}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	<-c
+	log.Println("Shutting down...")
+	srv.Shutdown(context.Background())
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome!\n")
+	fmt.Fprintf(w, "Welcome!\nVersion: %s\n ", version)
 }
